30a9092324acb866f9ef65951c8ee659ac5101ae
yuval11111111 <yuvalm4a@gmail.com>
2026-01-31 20:01:51 +0200

    removed unneeded code

--- console.js
+++ console.js
@@ -4,63 +4,26 @@ const readline = require('readline').createInterface({
     output: process.stdout
 });
 const {programCodeFile} = require("./start.js")
-let line = 1
 fs.readFile(programCodeFile, "utf8", (err, program) => {
-    for (let i = 0; i <= 0; i++) {
-        start()
-    }
-    if (program) {
-        readline.question(`Do yo want to write a new program or to run the current program\n 1->run current program\n 2->write a new program\n`, value => {
-            switch (value) {
-                case `1`:
-                    require(`./index`)
-                    break
-                case `2`:
-                    fs.writeFileSync(programCodeFile, ``)
-                    openWriter()
-                    break
-                default:
-                    process.exit()
-            }
-        })
-    } else {
-        openWriter()
-    }
+    openWriter()
+})
 
-    function start() {
-        if (program) {
-            readline.question(`Do yo want to write a new program or to run the current program\n 1->run current program\n 2->write a new program\n`, value => {
-                switch (value) {
-                    case `1`:
-                        require(`./index`)
-                        break
-                    case `2`:
-                        fs.writeFileSync(programCodeFile, ``)
-                        openWriter()
-                        break
-                    default:
-                        process.exit()
-                }
+
+let line = 1
+
+function openWriter() {
+    readline.question(`${line})`, value => {
+        if (value.toLowerCase() !== `close`) {
+            fs.readFile(programCodeFile, "utf8", (err, program) => {
+                let Code = (!program) ? `` : program + `\n`
+                fs.writeFileSync(programCodeFile, Code + value)
             })
-        } else {
+            line = line + 1
             openWriter()
         }
-    }
-
-    function openWriter() {
-        readline.question(`${line})`, value => {
-            if (value.toLowerCase() !== `close`) {
-                fs.readFile(programCodeFile, "utf8", (err, program) => {
-                    let Code = (!program) ? `` : program + `\n`
-                    fs.writeFileSync(programCodeFile, Code + value)
-                })
-                line = line + 1
-                openWriter()
-            }
-            else {
-                readline.close()
-                require(`./index`)
-            }
-        })
-    }
-})
+        else {
+            readline.close()
+            require(`./index`)
+        }
+    })
+}
